e0aead41d91b78099fac45c55f482a55337d7942
Daniel14gc <gon20293@uvg.edu.gt>
2022-10-29 19:06:21 -0600

    feat: Conway's game of life

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 #include "life.h"
+#include <cmath>
 
 using namespace std;
 
@@ -10,13 +11,141 @@ const float COLOR[4] = {1.0, 0.0, 0.0, 0.0};
 
 Life life(WIDTH, HEIGHT);
 
+bool isOutOfBounds(int x, int y)
+{
+    if (x < 0 || x >= WIDTH)
+    {
+        return true;
+    }
+    if (y < 0 || y >= HEIGHT)
+    {
+        return true;
+    }
+
+    return false;
+}
+
 void pixel(int x, int y)
 {
-    glEnable(GL_SCISSOR_TEST);
-    glScissor(x * 4, y * 4, 4, 4);
-    glClearColor(COLOR[0], COLOR[1], COLOR[2], COLOR[3]);
-    glClear(GL_COLOR_BUFFER_BIT);
-    glDisable(GL_SCISSOR_TEST);
+    if (!isOutOfBounds(x, y))
+    {
+        glEnable(GL_SCISSOR_TEST);
+        glScissor(x * 4, y * 4, 4, 4);
+        glClearColor(COLOR[0], COLOR[1], COLOR[2], COLOR[3]);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glDisable(GL_SCISSOR_TEST);
+    }
+}
+
+void hydro(int x, int y)
+{
+    life.setPixel(x, y);
+    life.setPixel(x + 1, y);
+    life.setPixel(x - 1, y);
+    life.setPixel(x, y - 3);
+    life.setPixel(x + 2, y - 2);
+    life.setPixel(x + 3, y);
+    life.setPixel(x + 2, y + 2);
+    life.setPixel(x, y + 3);
+    life.setPixel(x - 2, y + 2);
+    life.setPixel(x - 2, y);
+    life.setPixel(x - 2, y - 2);
+}
+
+void drawRightTopPulsar(int x, int y)
+{
+    life.setPixel(x, y);
+    life.setPixel(x, y - 1);
+    life.setPixel(x, y - 2);
+    life.setPixel(x + 1, y - 4);
+    life.setPixel(x + 2, y - 4);
+    life.setPixel(x + 3, y - 4);
+    life.setPixel(x + 1, y + 1);
+    life.setPixel(x + 2, y + 1);
+    life.setPixel(x + 3, y + 1);
+    life.setPixel(x + 5, y);
+    life.setPixel(x + 5, y - 1);
+    life.setPixel(x + 5, y - 2);
+}
+
+void drawLeftTopPulsar(int x, int y)
+{
+    life.setPixel(x, y);
+    life.setPixel(x, y - 1);
+    life.setPixel(x, y - 2);
+    life.setPixel(x - 1, y - 4);
+    life.setPixel(x - 2, y - 4);
+    life.setPixel(x - 3, y - 4);
+    life.setPixel(x - 1, y + 1);
+    life.setPixel(x - 2, y + 1);
+    life.setPixel(x - 3, y + 1);
+    life.setPixel(x - 5, y);
+    life.setPixel(x - 5, y - 1);
+    life.setPixel(x - 5, y - 2);
+}
+
+void drawRightBottomPulsar(int x, int y)
+{
+    life.setPixel(x, y - 1);
+    life.setPixel(x, y - 2);
+    life.setPixel(x, y - 3);
+    life.setPixel(x + 1, y - 4);
+    life.setPixel(x + 2, y - 4);
+    life.setPixel(x + 3, y - 4);
+    life.setPixel(x + 1, y + 1);
+    life.setPixel(x + 2, y + 1);
+    life.setPixel(x + 3, y + 1);
+    life.setPixel(x + 5, y);
+    life.setPixel(x + 5, y - 1);
+    life.setPixel(x + 5, y - 2);
+}
+
+void drawLeftBottomPulsar(int x, int y)
+{
+    life.setPixel(x, y - 1);
+    life.setPixel(x, y - 2);
+    life.setPixel(x, y - 3);
+    life.setPixel(x - 1, y - 4);
+    life.setPixel(x - 2, y - 4);
+    life.setPixel(x - 3, y - 4);
+    life.setPixel(x - 1, y + 1);
+    life.setPixel(x - 2, y + 1);
+    life.setPixel(x - 3, y + 1);
+    life.setPixel(x - 5, y);
+    life.setPixel(x - 5, y - 1);
+    life.setPixel(x - 5, y - 2);
+}
+
+
+
+void leftTopPulsar(int x, int y)
+{
+    drawLeftTopPulsar(x - 1, y - 2);
+}
+
+void rightTopPulsar(int x, int y)
+{
+    drawRightTopPulsar(x + 1, y - 2);
+}
+
+void leftBottomPulsar(int x, int y)
+{
+    drawLeftBottomPulsar(x - 1, y + 5);
+}
+
+void rightBottomPulsar(int x, int y)
+{
+    drawRightBottomPulsar(x + 1, y + 5);
+}
+
+
+
+void pulsar(int x, int y)
+{
+    leftTopPulsar(x, y);
+    leftBottomPulsar(x, y);
+    rightTopPulsar(x, y);
+    rightBottomPulsar(x, y);
 }
 
 void glider(int x, int y)
@@ -62,20 +191,6 @@ void createStable(int x, int y)
     life.setPixel(x + 1, y + 1);
 }
 
-bool isOutOfBounds(int x, int y)
-{
-    if (x < 0 || x >= WIDTH)
-    {
-        return true;
-    }
-    if (y < 0 || y >= HEIGHT)
-    {
-        return true;
-    }
-
-    return false;
-}
-
 bool isNeighbour(int neighbourX, int neighbourY, int identifier)
 {
     if (!isOutOfBounds(neighbourX, neighbourY))
@@ -87,6 +202,15 @@ bool isNeighbour(int neighbourX, int neighbourY, int identifier)
     }
     
     return false;
+}
+
+void initialConfiguration()
+{
+    hydro(20, 20);
+    hydro(20, 80);
+    hydro(80, 20);
+    hydro(80, 80);
+    pulsar(50, 50);
 }
 
 int getNeighbourCount(int x, int y, int identifier)
@@ -197,12 +321,14 @@ void updateLife()
 int main(void)
 {
     GLFWwindow* window;
-    createStable(20, 20);
+    /*createStable(20, 20);
     life.setPixel(40, 30);
-    life.setPixel(41, 30);
+    life.setPixl(41, 30);
     life.setPixel(42, 30);
     penta(30, 30);
-    updateLife();
+    hydro(70, 70);*/
+    initialConfiguration();
+    // updateLife();
 
     /* Initialize the library */
     if (!glfwInit())
